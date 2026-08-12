8a3dfa51779b3160f16a44ab2907b7da49a6a2cb
CristianaGiustiniani <cristianagiustiniani@gmail.com>
2026-08-12 21:25:57 +0200

    Fixed main class name and behavior when args are not provided

--- src/main/java/com/orto/logic/Launcher.java
+++ src/main/java/com/orto/logic/Launcher.java
@@ -3,7 +3,7 @@ package com.orto.logic;
 public class Launcher {
     public static void main(String[] args) {
         if (args == null || args.length < 2) {
-            args = new String[]{"DEMO", "GUI1"};
+            args = new String[]{"FULL", "GUI1"};
         }
         Main.main(args);
     }
