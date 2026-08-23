059190d34b4745227516603efc4a5d8ae9dbd280
CristianaGiustiniani <cristianagiustiniani@gmail.com>
2026-08-23 11:08:52 +0200

    Removed UserDAOFS dependendency from LoginController - assigned creation responsability to DAOFactory;

--- src/main/java/com/orto/logic/controller/LoginController.java
+++ src/main/java/com/orto/logic/controller/LoginController.java
@@ -1,9 +1,7 @@
 package com.orto.logic.controller;
 
-import com.orto.logic.model.dao.UserDAO;
 import com.orto.logic.model.dao.exceptions.*;
 import com.orto.logic.model.dao.factory.DAOFactory;
-import com.orto.logic.model.dao.filesystem.UserDAOFS;
 import com.orto.logic.model.entity.User;
 import com.orto.logic.model.entity.exceptions.WrongPasswordException;
 import com.orto.logic.utils.PersistenceType;
@@ -12,11 +10,9 @@ import com.orto.logic.utils.Session;
 
 public class LoginController {
     public void login(String email, String password, boolean rememberUser) throws ConnectionException, WrongPasswordException, WrongEmailException {
-        UserDAO userDAO;
         User user;
 
-        userDAO = DAOFactory.getDAOFactory().getUserDAO();
-        user = userDAO.getUser(email, password);
+        user = DAOFactory.getDAOFactory().getUserDAO().getUser(email, password);
         Session.getInstance().setLoggedUser(user);
 
         if (rememberUser) {
@@ -31,7 +27,7 @@ public class LoginController {
 
     public synchronized void tryAutologin() {
         try {
-            User user = UserDAOFS.getUser();
+            User user = DAOFactory.getDAOFactory(PersistenceType.FILESYSTEM).getUserDAO().getUser();
             Session.getInstance().setLoggedUser(user);
         } catch (NoRememberedUserException | ConnectionException ignored) {
             //if autologin can't be performed, it does not matter
@@ -40,7 +36,7 @@ public class LoginController {
 
     public static void logout() throws ForgetUserException {
         Session.getInstance().logout();
-        UserDAOFS.forgetUser();
+        DAOFactory.getDAOFactory(PersistenceType.FILESYSTEM).getUserDAO().forgetUser();
     }
 
 }
