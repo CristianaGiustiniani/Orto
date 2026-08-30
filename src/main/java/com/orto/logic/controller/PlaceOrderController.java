22da17aafaa7ddfd49ff9d737161707a27f69138
CristianaGiustiniani <cristianagiustiniani@gmail.com>
2026-08-30 15:54:08 +0200

    Added logic for ProcessPayment: added ProcessPaymentController controller, added PaymentGateway API/boundary, revised Payment and PlaceOrderController

--- src/main/java/com/orto/logic/controller/PlaceOrderController.java
+++ src/main/java/com/orto/logic/controller/PlaceOrderController.java
@@ -8,8 +8,6 @@ import com.orto.logic.model.dao.exceptions.ConnectionException;
 import com.orto.logic.model.dao.factory.DAOFactory;
 import com.orto.logic.model.entity.*;
 import com.orto.logic.model.entity.exceptions.NoProductSelectedException;
-import com.orto.logic.utils.PaymentStatus;
-import com.orto.logic.utils.PaymentType;
 
 import java.util.List;
 
@@ -55,26 +53,13 @@ public class PlaceOrderController {
     public void pay(PaymentBean paymentBean) throws FailedPaymentException {
         PaymentMapper mapper = new PaymentMapper();
         Payment payment = mapper.toEntity(paymentBean);
+        ProcessPaymentController paymentController = new ProcessPaymentController();
 
-        PaymentType type = payment.getPaymentType();
-
-        if (type == PaymentType.ONLINE) {
-            //dummy assignation: should redirect to payment gateway;
-            //in this dummy assignation, if i pay online, i get a failed payment
-
-            payment.setPaymentStatusFailed();
-            throw new FailedPaymentException();
-        } else if (type == PaymentType.CASH) {
-            payment.setPaymentStatusSuccessful();
-        }
-
-        if (payment.getPaymentStatus() == PaymentStatus.SUCCESSFUL) {
+        paymentController.processPayment(payment);
+        order.addPaymentInfo(payment);
+        if (order.isPaymentSuccessful()) {
             order.setOrderStatusPending();
         }
-        else {
-            order.setOrderStatusRejected();
-        }
-        order.addPaymentInfo(payment);
     }
 
     public void saveOrder() throws ConnectionException {
